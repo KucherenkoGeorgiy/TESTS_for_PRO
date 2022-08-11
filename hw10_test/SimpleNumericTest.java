40d52c218ac360cfd88a2bbea5c5d72d5a1297d2
KucherenkoGeorgiy <georgiy.kucherenko@gmail.com>
2022-08-11 12:37:27 +0300

    Adding home task 10

--- hw10_test/SimpleNumericTest.java
+++ hw10_test/SimpleNumericTest.java
@@ -4,52 +4,19 @@ import com.hillel.kucherenko.hw10.ArrayOfSimple;
 import com.hillel.kucherenko.hw10.MicroArray;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
 import java.util.Arrays;
 
 public class SimpleNumericTest {
-    final static ArrayOfSimple arrayOfSimple = new ArrayOfSimple(5, 100, 20);
+    final static int minValue = 5;
+    final static int maxValue = 1000;
+    final static int arraysQuantity = 50;
+    final static ArrayOfSimple arrayOfSimple = new ArrayOfSimple(minValue, maxValue, arraysQuantity);
 
-    {
+    static {
         arrayOfSimple.makeThreads();
     }
 
-    @Test
-    void whenTryToGetSimplesReceiveCorrectValues() {
-        int[] expectedArray = new int[]{};
-        int[] actualResult = arrayOfSimple.getFinalArray();
-        boolean isSimple;
-
-        Arrays.sort(actualResult);
-
-        for (int i = 5; i < 100; i++) {
-            isSimple = true;
-            for (int j = 2; j <= (i / 2); j++) {
-                if (i % j == 0) {
-                    isSimple = false;
-                    break;
-                }
-            }
-            if (isSimple) {
-                expectedArray = pushElementToArray(expectedArray, i);
-            }
-        }
-        Assertions.assertEquals(Arrays.toString(expectedArray), Arrays.toString(actualResult));
-    }
-
-    private int[] pushElementToArray(int[] expectedArray, int i) {
-        int[] tempArray = new int[]{};
-        if (expectedArray == null) {
-            expectedArray = new int[]{i};
-        } else {
-            tempArray = new int[expectedArray.length + 1];
-            for (int j = 0; j < expectedArray.length; j++) {
-                tempArray[j] = expectedArray[j];
-            }
-            tempArray[tempArray.length - 1] = i;
-        }
-        expectedArray = tempArray;
-        return expectedArray;
-    }
 
     @Test
     void checkIfArrayConsistsOfSubarraysCorrectly() {
@@ -95,4 +62,43 @@ public class SimpleNumericTest {
         System.out.println(Arrays.toString(tempRes));
         return tempRes;
     }
+
+    @Test
+    void whenTryToGetSimplesReceiveCorrectValues() {
+        int[] expectedArray = new int[]{};
+        int[] actualResult = Arrays.copyOf(arrayOfSimple.getFinalArray(), arrayOfSimple.getFinalArray().length);
+        boolean isSimple;
+
+        Arrays.sort(actualResult);
+
+        for (int i = minValue; i < maxValue; i++) {
+            isSimple = true;
+            for (int j = 2; j <= (i / 2); j++) {
+                if (i % j == 0) {
+                    isSimple = false;
+                    break;
+                }
+            }
+            if (isSimple) {
+                expectedArray = pushElementToArray(expectedArray, i);
+            }
+        }
+        Assertions.assertEquals(Arrays.toString(expectedArray), Arrays.toString(actualResult));
+    }
+
+    private int[] pushElementToArray(int[] expectedArray, int i) {
+        int[] tempArray = new int[]{};
+        if (expectedArray == null) {
+            expectedArray = new int[]{i};
+        } else {
+            tempArray = new int[expectedArray.length + 1];
+            for (int j = 0; j < expectedArray.length; j++) {
+                tempArray[j] = expectedArray[j];
+            }
+            tempArray[tempArray.length - 1] = i;
+        }
+        expectedArray = tempArray;
+        return expectedArray;
+    }
+
 }
